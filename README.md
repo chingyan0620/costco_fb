f067d02b21c511e9f3ff4e1b8f6d51daccb74543
chingyan0620 <10667013apple@gmail.com>
2023-04-09 19:31:19 +0800

    temp.

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# costco_fb
- data struct: it may use json format.
-    {"post_name":name,"content:"text in there",comments:[{"comment_name":name,"message":"text in there"}],"comment_number":X,"like_number":X}
+# Facebook costco group crawler
+
+
+This is a small project to crawl facebook group page. <br>
+
+
+## Explain
+I use selenium package of python to do this work,and used browser is Firefox.So there most to have a geckodriver in file, defult position is in same folder.<br>
+
+main.py is main program file,if you want to change crawled web, you have to change url in here. But I don't konw whether each facebook group is same archive ,user have to check before run. It's just a rough status.<br>
+sub_program file is the folder where main program's function place.<br>
+data file will put text file of the crawl result. 
+I use "a" tag to write in there, if you wnat to re-run this program, please remember there is some data in there.<br>
+
+## Data format
+
+Result data will save in ./data/selenium_fb_costco.txt.<br>
+You can change any path you want in main.py. And I use dictionary struct to store ,hope this will good to visualize.<br>
+```
+{
+   "post_name" : poster name,
+   "contnet" : contnet,
+   "comments" : [
+      {"comment_name" : leave comment user name,
+       "message" : leave comment }
+         .
+         .
+         .
+   ]
+}
+```
+Each line will be one post record.
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -7,31 +7,51 @@ from sub_program.get_data import get_commet
 options = Options()
 options.headless = True
 
- 
-firefox = webdriver.Firefox(options=options,executable_path="geckodriver")
+firefox = webdriver.Firefox(options=options, executable_path="geckodriver")
 firefox.get("https://www.facebook.com/groups/1260448967306807/")
 time.sleep(2)
 
-content = firefox.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[4]/div/div/div[2]/div/div")
-
+content = firefox.find_element(
+    By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[\
+        1]/div[4]/div/div/div[2]/div/div")
 
 get_post_counter = 0
+old_dict = {}
+point = 0
 while get_post_counter < 5:
-    post_block = content.find_elements(By.XPATH,"./div/div[2]/div[2]/*")
-    for i in post_block:
-        print("--------")
-        try:
-            if i.text.strip() != "" :
-                result = get_commet(i,firefox)
-                get_post_counter += 1
-            with open("./data/selenium_fb_costco.txt","a") as f:
-                f.write(str(result) +"\n")
-        except Exception as e:
-            # print(e)
-            # print("element remove!!")
-            # scroll page and recatch element
-            firefox.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-    
-firefox.quit()
+    post_block = content.find_elements(
+        By.CSS_SELECTOR, "div[class='x1yztbdb \
+            x1n2onr6 xh8yej3 x1ja2u2z']")
 
+    if point < len(post_block):
+        i = post_block[point]
+        point += 1
+        try:
+            if i.text.strip() != "":
+                result = get_commet(i, firefox)
+                if not old_dict:
+                    old_dict = result
+                    get_post_counter += 1
+                    with open("./data/selenium_fb_costco.txt", "a") as f:
+                        f.write(str(result) + "\n")
+                else:
+                    if old_dict["post_name"] == result["post_name"]:
+                        continue
+                    else:
+                        old_dict = result
+                        get_post_counter += 1
+                        with open("./data/selenium_fb_costco.txt", "a") as f:
+                            f.write(str(result) + "\n")
+        except Exception as e:
+            if i == post_block[-1]:
+                firefox.execute_script(
+                    "window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+            else:
+                pass
+    else:
+        firefox.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+
+firefox.quit()
